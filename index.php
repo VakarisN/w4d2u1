e3c0ff43fc96e3ad9ec900720554a1a587a349b9
VakarisN <vakarisneverauskas@gmail.com>
2019-04-30 19:26:21 -0700

    dar daugiau php kodo, datus variacijos

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <link rel="stylesheet" href="normalized.css">
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
-    <title>Uzduotis 1 <?php print 'PHP'; ?></title>
+    <title><?php print 'PHP' . date('l'); ?></title>
   </head>
   <body>
-    <h1> Vakaris - HTML asas</h1>
+    <h1> Vakaris - HTML <?php print 'ir PHP asas jau nuo '. date ('Y');?> </h1>
     <p>As valdau <span>HTML</span> ir <?php print 'PHP'; ?> </p>
-    <p>Kuriu dinamini turini <?php print date('Y-m-d h:i:s')?></p>
+    <p><?php print 'Kuriu dinamini turini ' . date('Y-m-d h:i:s')?></p>
+    <p>Viskas prasidejo nuo <?php print date('F-d');?></p>
   </body>
 </html>
