e73d30a62dbbefc6e22d9925b458be856837a53a
VakarisN <vakarisneverauskas@gmail.com>
2019-04-30 19:04:48 -0700

    Yra php kodo, prideta data

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <link rel="stylesheet" href="normalized.css">
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
-    <title>Uzduotis 1</title>
+    <title>Uzduotis 1 <?php print 'PHP'; ?></title>
   </head>
   <body>
     <h1> Vakaris - HTML asas</h1>
-    <p>As valdau HTML</p>
+    <p>As valdau <span>HTML</span> ir <?php print 'PHP'; ?> </p>
+    <p>Kuriu dinamini turini <?php print date('Y-m-d h:i:s')?></p>
   </body>
 </html>
